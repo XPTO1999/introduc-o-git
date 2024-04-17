135faa4ccc7b844c373c19a1d9b67b626a2401ab
XPTO1999 <166664854+XPTO1999@users.noreply.github.com>
2024-04-17 22:35:40 +0000

    resolvendo atividade

---
+++ atividade.py
@@ -0,0 +1,8 @@
+# Crie uma variável idade e uma condicional que diga se a pessoa é maior de idade ou menor de idade.
+
+idade = 18 
+
+if idade >= 18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
